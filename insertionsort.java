c263f15593bf6b9872d0a3cc0e3d9f96bc1c17a0
unknown <harshbhardwaj1003@gmail.com>
2025-04-04 23:27:25 +0530

    insertionsort in java

---
+++ insertionsort.java
@@ -0,0 +1,21 @@
+public class insertionsort{
+    public static void main(String[] args) {
+        int [] arr = {  8 , 3 , 6 , 2 , 4, 10 , 7};
+        int n = arr.length;
+        for(int  i= 1; i<n; i++){
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 &&  arr[j]>key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+
+        } System.out.println("sorted array is   :       ");
+
+         for(int i=0; i<n; i++){
+            System.out.println(arr[i] +   "    ");
+         }
+        
+    }
+}
